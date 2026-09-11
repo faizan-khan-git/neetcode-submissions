e062c1a57e48e7f7c3988c5ca8194c823404a89c
faizan-khan-git <mdfaizank2004@gmail.com>
2026-09-11 21:44:28 +0530

    Add: pow-x-n - submission-3

---
+++ Data Structures & Algorithms/pow-x-n/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double result = 1.0;
+        long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        while(N > 0){
+            if(N % 2 != 0){
+                result *= x;
+            }
+
+            x *= x;
+            N /= 2;
+        }
+
+        return result;
+    }
+}
